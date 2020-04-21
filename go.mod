958303f7178f76e6b94c1636bf502ca092e8d395
kenanya gusti <khent.jazu@gmail.com>
2020-04-21 21:18:00 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kenanya/breaking-the-records
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the breakingRecords function below.
+func breakingRecords(scores []int32) []int32 {
+	
+	highest, lowest :=  scores[0], scores[0]
+	var highMember, lowMember []int32
+	for ii:=0; ii<len(scores); ii++ {
+		if scores[ii] > highest {
+			highMember = append(highMember, scores[ii])
+			highest = scores[ii]
+		} else if scores[ii] < lowest {
+			lowMember = append(lowMember, scores[ii])
+			lowest = scores[ii]
+		}
+	}
+
+	return []int32{int32(len(highMember)), int32(len(lowMember))}
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    scoresTemp := strings.Split(readLine(reader), " ")
+
+    var scores []int32
+
+    for i := 0; i < int(n); i++ {
+        scoresItemTemp, err := strconv.ParseInt(scoresTemp[i], 10, 64)
+        checkError(err)
+        scoresItem := int32(scoresItemTemp)
+        scores = append(scores, scoresItem)
+    }
+
+    result := breakingRecords(scores)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, " ")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
